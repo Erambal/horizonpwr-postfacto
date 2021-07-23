8c3a8c31e443834851b837048f3d9680eab0b7c4
Kendrikc Rambal <k.rambal96@gmail.com>
2021-07-23 13:05:18 -0600

    First Commit

---
+++ heroku/config/config.js
@@ -0,0 +1,17 @@
+window.Retro = {
+  config: {
+    "globalNamespace": "Retro",
+    "title": "Postfacto",
+    "scripts": ["application.js"],
+    "stylesheets": ["application.css"],
+    "useRevManifest": true,
+    "api_base_url": "/api",
+    "websocket_url": "/cable",
+    "enable_analytics": false,
+    "contact": "",
+    "terms": "",
+    "privacy": "",
+    "google_oauth_client_id": 547890254003-e6d8q8lqgssc85ec2dl15t4qs6vh8pnb.apps.googleusercontent.com
+  }
+};
+
